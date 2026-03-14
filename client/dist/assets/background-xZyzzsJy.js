84f474d7510de1eabecb61483d9f119d849d57c3
vinothkumar-2596 <design@smvec.ac.in>
2026-03-14 19:00:58 +0530

    Stop tracking client build output

--- client/dist/assets/background-xZyzzsJy.js
+++
@@ -1,6 +0,0 @@
-import{c as n,a as t}from"./index-DD_RDmYl.js";import{j as e}from"./vendor-ui-CWNJASLM.js";/**
- * @license lucide-react v0.462.0 - ISC
- *
- * This source code is licensed under the ISC license.
- * See the LICENSE file in the root directory of this source tree.
- */const x=n("LayoutGrid",[["rect",{width:"7",height:"7",x:"3",y:"3",rx:"1",key:"1g98yp"}],["rect",{width:"7",height:"7",x:"14",y:"3",rx:"1",key:"6d4xhi"}],["rect",{width:"7",height:"7",x:"14",y:"14",rx:"1",key:"nxv5o0"}],["rect",{width:"7",height:"7",x:"3",y:"14",rx:"1",key:"1bb6yr"}]]);function c({children:r,className:a,hideGrid:i=!1}){return e.jsxs("div",{className:t("relative w-full",a),children:[e.jsx("div",{className:"pointer-events-none absolute inset-0 flex items-center justify-center dark:bg-black bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]",children:e.jsx("div",{className:t("absolute inset-0 bg-none bg-[size:20px_20px]",i?"dark:bg-none":"dark:bg-[linear-gradient(to_right,#80808012_1px,transparent_1px),linear-gradient(to_bottom,#80808012_1px,transparent_1px)]")})}),r]})}export{c as G,x as L};
